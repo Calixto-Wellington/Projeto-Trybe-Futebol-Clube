1087b0659051edd29721dbcc3712ec5c25237c52
Calixto-Wellington <wellington1081@gmail.com>
2022-06-13 14:16:47 -0300

    Requisitos 20 e 21 ok

--- app/backend/src/controller/MatchController.ts
+++ app/backend/src/controller/MatchController.ts
@@ -2,9 +2,15 @@ import { Request, Response, NextFunction } from 'express';
 import MatchService from '../services/MatchService';
 
 export default class MatchController {
-  static async getAll(_req: Request, res: Response, next: NextFunction) {
+  static async getAll(req: Request, res: Response, next: NextFunction) {
     try {
-      const match = await MatchService.getAll();
+      const { inProgress } = req.query;
+      const progressInConvert = inProgress === 'true';
+      if (inProgress) {
+        const match = await MatchService.getAll(progressInConvert);
+        return res.status(200).json(match);
+      }
+      const match = await MatchService.getAllLala();
       return res.status(200).json(match);
     } catch (error) {
       return next(error);
--- app/backend/src/services/MatchService.ts
+++ app/backend/src/services/MatchService.ts
@@ -2,11 +2,28 @@ import TeamModel from '../database/models/TeamModel';
 import MatchModel from '../database/models/MatchModel';
 
 export default class MatchService {
-  static async getAll() {
+  static async getAll(params: boolean) {
     const match = await MatchModel.findAll({
+      where: { inProgress: params },
+      include: [{ model: TeamModel,
+        as: 'teamHome',
+        attributes: { exclude: ['id'] },
+      },
+      {
+        model: TeamModel,
+        as: 'teamAway',
+        attributes: { exclude: ['id'] },
+      },
+      ],
+    });
+    return match;
+  }
+  // this.getAllLala();
+
+  static async getAllLala() {
+    const match1 = await MatchModel.findAll({
       include: [
-        {
-          model: TeamModel,
+        { model: TeamModel,
           as: 'teamHome',
           attributes: { exclude: ['id'] },
         },
@@ -17,7 +34,6 @@ export default class MatchService {
         },
       ],
     });
-
-    return match;
+    return match1;
   }
 }
